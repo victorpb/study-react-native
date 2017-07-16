921c8490d82bea725614548ede4341b578146665
Victor Pereira <victtor.vpb@gmail.com>
2017-07-16 14:31:23 -0300

    create component text

---
+++ application/components/Text.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Text} from 'react-native';
+
+export default class TextComponent extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <Text> { this.props.MyText } </Text>
+        )
+    }
+}
